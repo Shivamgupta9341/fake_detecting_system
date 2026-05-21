00cd21abf06d48c81e578798620120a73190eaa9
SHIVAM GUPTA <shivamkumarsawnaugadh@gmail.com>
2026-05-21 11:33:50 +0530

    Update register.py

--- fake_email/pages/register.py
+++ fake_email/pages/register.py
@@ -31,8 +31,9 @@ if b1:
               
               my.insert_one({"name":name,"password":password, "gender":g,"Email":email,"dob":str(dob)})
               st.success("Your data are saved!!")
-              st.switch_page("pages/profile.py")
-
+              for data in str:
+                     st.session_state["name"]=data['name']
+                     st.switch_page("pages/profile.py")
 
 
 
